4e5940aef9002a987c080444e9ded8c62da37668
Nikita-Koval <nkoval.exceedteam@gmail.com>
2021-08-31 12:01:17 +0300

    add routes and controllers for users list. add filter by age and gender

--- routes/users/getUserInfo.js
+++ routes/users/getUserInfo.js
@@ -3,5 +3,6 @@ const userController = require("../../controllers/users/users");
 const router = express.Router();
 
 router.get("/get-user-info", userController.getUserInfo);
+router.get("/get-users-list", userController.getUserList);
 
 module.exports = router;
